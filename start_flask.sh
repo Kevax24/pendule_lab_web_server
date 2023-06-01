75f8eea8bacc1039afee8fd1e1d111df9ea535bd
Xavier Clivaz <xavier.clivaz@master.hes-so.ch>
2023-06-01 16:48:54 +0200

    Edit server name

--- start_flask.sh
+++ start_flask.sh
@@ -5,6 +5,6 @@ pip install -r requirements.txt
 export FLASK_APP=app
 export FLASK_DEBUG=false
 export FLASK_ENV=production
-export SERVER_NAME='pendule1.local:5000'
+export SERVER_NAME='pendule1.lab:5000'
 
 flask run --host=0.0.0.0
